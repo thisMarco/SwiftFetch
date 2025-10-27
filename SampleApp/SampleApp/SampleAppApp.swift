727ddd7e6bbecb60c6f6d7ff67d8f1722fbb3d0c
thisMarco <55879616+thisMarco@users.noreply.github.com>
2025-10-27 11:28:11 +0000

    Created Empty SampleApp

---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Marco Picchillo on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
